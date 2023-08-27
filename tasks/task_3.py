9885ce338085df994bdc1122b3d4216d305ad587
Konstantin Kotov <konstantin.kotov1993@mail.ru>
2023-08-27 13:43:12 +0300

    TEST
    * test push

--- tasks/task_3.py
+++ tasks/task_3.py
@@ -1,7 +1,7 @@
 string_literal = "hello"
 integer_literal = 12
-float_literal = 3.5
-boolean_literal = False
+float_literal = 3.3
+boolean_literal = True
 
 print(string_literal)
 print(integer_literal)
